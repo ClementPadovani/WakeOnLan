bc68f4a74765b32de59feb3152ff0e9d7bfa986f
Clement <clement.padovani@me.com>
2015-07-31 16:41:51 +0200

    Added MAC address formatter.

---
+++ WakeOnLan/WOLMACAddressFormatter.h
@@ -0,0 +1,13 @@
+//
+//  WOLMACAddressFormatter.h
+//  WakeOnLan
+//
+//  Created by Clément Padovani on 7/31/15.
+//  Copyright © 2015 Clément Padovani. All rights reserved.
+//
+
+@import Foundation;
+
+@interface WOLMACAddressFormatter : NSFormatter
+
+@end
---
+++ WakeOnLan/WOLMACAddressFormatter.m
@@ -0,0 +1,13 @@
+//
+//  WOLMACAddressFormatter.m
+//  WakeOnLan
+//
+//  Created by Clément Padovani on 7/31/15.
+//  Copyright © 2015 Clément Padovani. All rights reserved.
+//
+
+#import "WOLMACAddressFormatter.h"
+
+@implementation WOLMACAddressFormatter
+
+@end
