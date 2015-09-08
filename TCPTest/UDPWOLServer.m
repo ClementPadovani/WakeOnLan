0689b5c461348a481b6805b581fd536b2d3870bd
Clement <clement.padovani@me.com>
2015-09-08 14:55:52 +0200

    Added MAC addresse retriever.

--- TCPTest/UDPWOLServer.m
+++ TCPTest/UDPWOLServer.m
@@ -10,7 +10,14 @@
 
 #import <CocoaAsyncSocket/CocoaAsyncSocket.h>
 
+#include <sys/socket.h>
+#include <sys/sysctl.h>
+#include <net/if.h>
+#include <net/if_dl.h>
+
 @interface UDPWOLServer () <GCDAsyncUdpSocketDelegate>
+
+@property (nonatomic, weak, readwrite, nullable) id <UDPWOLServerDelegate> delegate;
 
 @property (nonatomic, strong) GCDAsyncUdpSocket *socket;
 
@@ -44,6 +51,73 @@
 
 @implementation UDPWOLServer
 
+- (NSString *)getMacAddress
+{
+	int                 mgmtInfoBase[6];
+	char                *msgBuffer = NULL;
+	size_t              length;
+	unsigned char       macAddress[6];
+	struct if_msghdr    *interfaceMsgStruct;
+	struct sockaddr_dl  *socketStruct;
+	NSString            *errorFlag = NULL;
+	
+	// Setup the management Information Base (mib)
+	mgmtInfoBase[0] = CTL_NET;        // Request network subsystem
+	mgmtInfoBase[1] = AF_ROUTE;       // Routing table info
+	mgmtInfoBase[2] = 0;
+	mgmtInfoBase[3] = AF_LINK;        // Request link layer information
+	mgmtInfoBase[4] = NET_RT_IFLIST;  // Request all configured interfaces
+	
+	// With all configured interfaces requested, get handle index
+	if ((mgmtInfoBase[5] = if_nametoindex("en0")) == 0)
+		errorFlag = @"if_nametoindex failure";
+	else
+	{
+		// Get the size of the data available (store in len)
+		if (sysctl(mgmtInfoBase, 6, NULL, &length, NULL, 0) < 0)
+			errorFlag = @"sysctl mgmtInfoBase failure";
+		else
+		{
+			// Alloc memory based on above call
+			if ((msgBuffer = malloc(length)) == NULL)
+				errorFlag = @"buffer allocation failure";
+			else
+			{
+				// Get system information, store in buffer
+				if (sysctl(mgmtInfoBase, 6, msgBuffer, &length, NULL, 0) < 0)
+					errorFlag = @"sysctl msgBuffer failure";
+			}
+		}
+	}
+	
+	// Befor going any further...
+	if (errorFlag != NULL)
+	{
+		NSLog(@"Error: %@", errorFlag);
+		return errorFlag;
+	}
+	
+	// Map msgbuffer to interface message structure
+	interfaceMsgStruct = (struct if_msghdr *) msgBuffer;
+	
+	// Map to link-level socket structure
+	socketStruct = (struct sockaddr_dl *) (interfaceMsgStruct + 1);
+	
+	// Copy link layer address data in socket structure to an array
+	memcpy(&macAddress, socketStruct->sdl_data + socketStruct->sdl_nlen, 6);
+	
+	// Read from char array into a string object, into traditional Mac address format
+	NSString *macAddressString = [NSString stringWithFormat:@"%02X:%02X:%02X:%02X:%02X:%02X",
+							macAddress[0], macAddress[1], macAddress[2],
+							macAddress[3], macAddress[4], macAddress[5]];
+	NSLog(@"Mac Address: %@", macAddressString);
+	
+	// Release the buffer memory
+	free(msgBuffer);
+	
+	return macAddressString;
+}
+
 - (instancetype) init
 {
 	self = [super init];
@@ -56,8 +130,12 @@
 	return self;
 }
 
-- (void) setup
+- (void) setupWithDelegate: (id <UDPWOLServerDelegate>) delegate
 {
+	NSParameterAssert(delegate);
+	
+	[self setDelegate: delegate];
+	
 	GCDAsyncUdpSocket *socket = [[GCDAsyncUdpSocket alloc] initWithDelegate: self delegateQueue: dispatch_get_main_queue()];
 	
 	NSError *error = nil;
@@ -77,6 +155,15 @@
 	[self setSocket: socket];
 }
 
+- (void) tearDown
+{	
+	[[self socket] close];
+	
+	[self setSocket: nil];
+	
+	[self setDelegate: nil];
+}
+
 - (void) udpSocket:(GCDAsyncUdpSocket *)sock didNotConnect:(NSError *)error
 {
 	NSLog(@"did not connect: %@", error);
@@ -89,19 +176,11 @@
 
 - (void) udpSocket:(GCDAsyncUdpSocket *)sock didReceiveData:(NSData *)data fromAddress:(NSData *)address withFilterContext:(id)filterContext
 {
-	
-	NSString *host = nil;
-	
-	uint16_t port;
-	
-	if ([GCDAsyncUdpSocket getHost: &host port: &port fromAddress: address])
-	{
-		NSLog(@"host: %@", host);
-		
-		NSLog(@"port: %u", port);
-	}
-	
 	NSLog(@"received: %@", [data hexString]);
+	
+	[[self delegate] wolServer: self
+		 didReceiveDataString: [data hexString]
+			  fromMACAddress: [self getMacAddress]];
 }
 
 
