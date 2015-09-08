a875a7158950701a88e43826b2fdd268cdf2ab1a
Clement <clement.padovani@me.com>
2015-09-08 14:55:26 +0200

    Added server spec.

---
+++ TCPTest/UDPWOLServerSpec.m
@@ -0,0 +1,19 @@
+//
+//  UDPWOLServerSpec.m
+//  WakeOnLan
+//
+//  Created by Clément Padovani on 9/8/15.
+//  Copyright 2015 Clément Padovani. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "UDPWOLServer.h"
+
+
+SPEC_BEGIN(UDPWOLServerSpec)
+
+describe(@"UDPWOLServer", ^{
+
+});
+
+SPEC_END
