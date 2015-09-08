8937674c2ace4a6a31ff07769a1d2a643ccfdd91
Clement <clement.padovani@me.com>
2015-09-08 11:47:13 +0200

    Installed Fabric.

--- WakeOnLan/WOLAppDelegate.m
+++ WakeOnLan/WOLAppDelegate.m
@@ -6,13 +6,22 @@
 //  Copyright © 2015 Clément Padovani. All rights reserved.
 //
 
+#ifdef RELEASE
+@import Fabric;
+@import Crashlytics;
+#endif
+
 #import "WOLAppDelegate.h"
 
 @implementation WOLAppDelegate
 
 - (void) applicationDidFinishLaunching:(nonnull NSNotification *)notification
 {
-
+	#ifdef RELEASE
+		[[NSUserDefaults standardUserDefaults] registerDefaults:@{ @"NSApplicationCrashOnExceptions": @YES }];
+	
+		[Fabric with:@[[Crashlytics class]]];
+	#endif
 }
 
 - (BOOL) applicationShouldTerminateAfterLastWindowClosed: (nonnull NSApplication *) sender
