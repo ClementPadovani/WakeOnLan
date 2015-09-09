6bbfea9e693d021221a158e7877431fc6237aaf5
Clement <clement.padovani@me.com>
2015-09-09 14:14:54 +0200

    Added history stuff.

--- WakeOnLan/WOLAppDelegate.m
+++ WakeOnLan/WOLAppDelegate.m
@@ -14,10 +14,15 @@
 #import "WOLAppDelegate.h"
 #import "WOLHistoryManager.h"
 
+#import <SNRFetchedResultsController/SNRFetchedResultsController.h>
+#import "WOLHistoryItem.h"
 
-@interface WOLAppDelegate ()
+
+@interface WOLAppDelegate () <SNRFetchedResultsControllerDelegate>
 
 @property (weak) IBOutlet NSMenuItem *historyMenuItem;
+
+@property (strong) SNRFetchedResultsController *fetchedResultsController;
 
 @end
 
@@ -32,7 +37,56 @@
 		[Fabric with:@[[Crashlytics class]]];
 	#endif
 	
+	NSManagedObjectContext *context = [[WOLHistoryManager sharedManager] mainContext];
 	
+	NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName: [WOLHistoryItem entityName]];
+	
+	NSSortDescriptor *lastUsedDateSortDescriptor = [NSSortDescriptor sortDescriptorWithKey: NSStringFromSelector(@selector(lastUsedDate))
+														ascending: YES];
+	
+	[fetchRequest setSortDescriptors: @[lastUsedDateSortDescriptor]];
+	
+	SNRFetchedResultsController *fetchedResultsController = [[SNRFetchedResultsController alloc] initWithManagedObjectContext: context
+																						    fetchRequest: fetchRequest];
+	
+	[fetchedResultsController setDelegate: self];
+	
+	NSError *fetchError = nil;
+	
+	if (![fetchedResultsController performFetch: &fetchError])
+	{
+		CPLog(@"fetch error: %@", fetchError);
+		
+		[NSApp presentError: fetchError];
+	}
+}
+
+- (void) controllerDidChangeContent: (SNRFetchedResultsController *) controller
+{
+	NSMenu *historyMenu = [[self historyMenuItem] menu];
+	
+	[historyMenu removeAllItems];
+	
+	for (WOLHistoryItem *aHistoryItem in [controller fetchedObjects])
+	{
+		NSMenuItem *menuItem = [[NSMenuItem alloc] initWithTitle: [aHistoryItem macAddress]
+												action: NULL
+										   keyEquivalent: @""];
+		
+		[historyMenu addItem: menuItem];
+	}
+	
+	if (![historyMenu itemArray] ||
+	    ![[historyMenu itemArray] count])
+	{
+		NSMenuItem *noItemsMenuItem = [[NSMenuItem alloc] initWithTitle: @"No History"
+													  action: NULL
+												keyEquivalent: @""];
+		
+		[noItemsMenuItem setEnabled: NO];
+		
+		[historyMenu addItem: noItemsMenuItem];
+	}
 }
 
 - (BOOL) applicationShouldTerminateAfterLastWindowClosed: (nonnull NSApplication *) sender
