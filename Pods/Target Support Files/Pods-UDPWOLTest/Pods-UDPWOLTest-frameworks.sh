b446dfc531587c2e51b49b0b6aab18265173fb3b
Clement <clement.padovani@me.com>
2015-09-09 17:21:14 +0200

    As-is.

--- Pods/Target Support Files/Pods-UDPWOLTest/Pods-UDPWOLTest-frameworks.sh
+++ Pods/Target Support Files/Pods-UDPWOLTest/Pods-UDPWOLTest-frameworks.sh
@@ -80,3 +80,10 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-UDPWOLTest/CocoaAsyncSocket.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-UDPWOLTest/CocoaAsyncSocket.framework"
+fi
