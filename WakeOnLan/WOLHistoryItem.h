a7dce286d14b537be4900776fbd4cbadafa24258
Clement <clement.padovani@me.com>
2015-09-09 13:25:23 +0200

    Made WOLHistoryItem a base object subclass.

--- WakeOnLan/WOLHistoryItem.h
+++ WakeOnLan/WOLHistoryItem.h
@@ -6,11 +6,11 @@
 //  Copyright © 2015 Clément Padovani. All rights reserved.
 //
 
-@import CoreData;
+#import "WOLBaseObject.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface WOLHistoryItem : NSManagedObject
+@interface WOLHistoryItem : WOLBaseObject
 
 // Insert code here to declare functionality of your managed object subclass
 
